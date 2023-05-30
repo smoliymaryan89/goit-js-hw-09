041b4bbdaafebfbafe3c6197f753989167cf14c9
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-05-30 13:27:10 +0300

    Create 03-promises.js

---
+++ src/js/03-promises.js
@@ -0,0 +1,30 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  for (let i = 1; i <= Number(amount.value); i++) {
+    createPromise(i, Number(step.value) * i + Number(delay.value))
+      .then(msg => Notify.success(msg))
+      .catch(error => Notify.failure(error));
+  }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
